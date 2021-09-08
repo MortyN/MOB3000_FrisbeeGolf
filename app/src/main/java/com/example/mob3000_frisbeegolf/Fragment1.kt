d711d594d3d051c80acbf476979b2108eeb8bae4
Morten Neteland <57175034+MortyN@users.noreply.github.com>
2021-09-08 16:11:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mob3000_frisbeegolf/Fragment1.kt
@@ -0,0 +1,13 @@
+package com.example.mob3000_frisbeegolf
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/mob3000_frisbeegolf/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.mob3000_frisbeegolf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    //lateinit will allow to skip the otherwise required initializer and will make the property access fail with exception until some meaningful value is assigned to it https://stackoverflow.com/questions/33849811/property-must-be-initialized-or-be-abstract
+    private lateinit var drawer: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this,drawer,toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if(savedInstanceState == null ){
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Fragment1()).commit()
+        navigationView.setCheckedItem(R.id.nav_message)
+        navigationView.setCheckedItem(R.id.nav_message)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        } else{
+            super.onBackPressed()
+        }
+        super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.nav_message -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Fragment1()).commit()
+            R.id.nav_chat -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Fragment2()).commit()
+            R.id.nav_profile -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Fragment3()).commit()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
