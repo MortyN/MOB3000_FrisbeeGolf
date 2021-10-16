07388d581b3346fe5e919545ebbd745580834499
Morten Neteland <57175034+MortyN@users.noreply.github.com>
2021-10-16 16:12:16 +0200

    reworked fragmen navigation and added navgraph instead

--- app/src/main/java/com/example/mob3000_frisbeegolf/activities/Main/MainActivity.kt
+++ app/src/main/java/com/example/mob3000_frisbeegolf/activities/Main/MainActivity.kt
@@ -1,83 +1,58 @@
 package com.example.mob3000_frisbeegolf.activities.Main
 
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mob3000_frisbeegolf.*
-import com.example.mob3000_frisbeegolf.activities.Feed.Feed
-import com.example.mob3000_frisbeegolf.activities.AddRound.view.AddRound
-import com.example.mob3000_frisbeegolf.activities.MyProfile.MyProfile
-import com.example.mob3000_frisbeegolf.activities.MyRounds.MyRounds
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     //lateinit will allow to skip the otherwise required initializer and will make the property access fail with exception until some meaningful value is assigned to it https://stackoverflow.com/questions/33849811/property-must-be-initialized-or-be-abstract
     private lateinit var drawer: DrawerLayout
     private lateinit var bottomNavigationView: BottomNavigationView
-    private var prevFragment: Int = 0
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //disable night mode color theme
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
+        //disable night mode color theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        toolbar = findViewById(R.id.toolbar)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
         drawer = findViewById(R.id.drawer_layout)
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.fragment_feed, R.id.fragment_myrounds, R.id.fragment_addround, R.id.fragment_myprofile),
+            drawer
         )
 
-        drawer.addDrawerListener(toggle)
-        toggle.syncState()
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        if (savedInstanceState == null) {
+        bottomNavigationView.setupWithNavController(navController)
 
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, Feed())
-                .commit()
-            navigationView.setCheckedItem(R.id.nav_myprofile)
-            navigationView.setCheckedItem(R.id.nav_myprofile)
-        }
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavigationView.setOnItemSelectedListener(navListener)
+
     }
 
-    private val navListener =
-        BottomNavigationView.OnNavigationItemSelectedListener { item -> // By using switch we can easily get
-            // the selected fragment
-            // by using there id.
-            var selectedFragment: Fragment? = null
-            when (item.itemId) {
-                R.id.feed_navbtn -> selectedFragment = Feed()
-                R.id.myrounds_navbtn -> selectedFragment = MyRounds()
-                R.id.addround_navbtn -> selectedFragment = AddRound()
-                R.id.myprofile_navbtn -> selectedFragment = MyProfile()
-            }
-            // It will help to replace the
-            // one fragment to other.
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment!!)
-                .commit()
-            true
-        }
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
     override fun onBackPressed() {
         val count = supportFragmentManager.backStackEntryCount
@@ -94,47 +69,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             supportFragmentManager.popBackStack();
             return
         }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        when (item.itemId) {
-//            R.id.nav_myprofile -> supportFragmentManager.beginTransaction().replace(
-//                R.id.fragment_container,
-//                MyProfile()
-//            ).addToBackStack(null).commit()
-//            R.id.nav_friends -> supportFragmentManager.beginTransaction().replace(
-//                R.id.fragment_container,
-//                MyRounds()
-//            ).addToBackStack(null).commit()
-//            R.id.nav_arenarecords -> supportFragmentManager.beginTransaction().replace(
-//                R.id.fragment_container,
-//                AddRound()
-//            ).addToBackStack(null).commit()
-//            R.id.feed_navbtn -> supportFragmentManager.beginTransaction().replace(
-//                R.id.fragment_container,
-//                MyProfile()
-//            ).addToBackStack(null).commit()
-//        }
-        // the selected fragment
-        // by using there id.
-        var selectedFragment: Fragment? = null
-        when (item.itemId) {
-            R.id.nav_myprofile -> selectedFragment = Feed()
-            R.id.nav_friends -> selectedFragment = MyRounds()
-            R.id.nav_arenarecords -> selectedFragment = AddRound()
-            R.id.nav_addarena -> selectedFragment = MyProfile()
-            R.id.nav_yourarenas -> selectedFragment = MyProfile()
-            R.id.nav_settings -> selectedFragment = MyProfile()
-        }
-        // It will help to replace the
-        // one fragment to other.
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, selectedFragment!!)
-            .commit()
-        true
-        drawer.closeDrawer(GravityCompat.START)
-        return true
     }
 }
 
